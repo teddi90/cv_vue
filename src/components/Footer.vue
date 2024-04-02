0e7fe957620b5475dadb634e889fef3ac60a971e
teddi90 <slobodyanoleg5@gmail.com>
2024-04-02 17:03:54 +0300

    chnge footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,9 +3,9 @@
 </script>
 
 <template>
-  <footer class="bg-mainColor pt-[30px]">
+  <footer class="bg-mainColor pt-[30px] pb-[15px]">
     <div class="container mx-auto">
-      <a href="#home" class="inline-flex  items-center text-white">
+      <a href="#home" class="inline-flex  items-center text-white mb-5">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
              stroke="currentColor" class="w-8 h-8 inline-block mr-1">
           <path stroke-linecap="round" stroke-linejoin="round"
@@ -13,34 +13,27 @@
         </svg>
         <span class="font-bold text-xl">frontend_developer</span>
       </a>
-      <ul class="footer__list">
-        <li>
-          <a href="#about" class="footer__link">About</a>
-        </li>
-        <li>
-          <a href="#work" class="footer__link">Project</a>
-        </li>
-        <li>
-          <a href="#skills" class="footer__link">Skills</a>
-        </li>
-      </ul>
-      <ul class="footer__social">
-        <li>
-          <a href="https://www.facebook.com/oleg510bodyan" target="_blank" class="footer__social-link">
-            <i class='bx bxl-facebook'></i>
-          </a>
-        </li>
-        <li>
-          <a href="https://www.instagram.com/olegs10bodyan/" target="_blank" class="footer__social-link">
-            <i class='bx bxl-instagram'></i>
-          </a>
-        </li>
-        <li>
-          <a href="https://linkedin.com/in/olegsloboydan" class="footer__social-link" target="_blank">
-            <i class='bx bxl-linkedin-square'></i>
-          </a>
-        </li>
-      </ul>
+      <div class="flex justify-end mb-40px items-center">
+        <p class="text-white mr-4 italic">Let's <span class="font-semibold not-italic">create</span> together</p>
+        <a href="https://linkedin.com/in/olegsloboydan"
+           class="group flex items-center p-[8px] rounded-full shadow-black m-3 hover:shadow-lg bg-white transition duration-500 ease-in-out"
+           target="_blank">
+          <font-awesome-icon :icon="['fab', 'linkedin']" class="h-6 w-6 group-hover:text-[#0e76a8] transition duration-500 ease-in-out"/>
+        </a>
+        <a href="https://github.com/teddi90"
+           class="group flex items-center p-[8px] rounded-full shadow-black m-3 hover:shadow-lg bg-white transition duration-500 ease-in-out"
+           target="_blank">
+          <font-awesome-icon :icon="['fab', 'github']" class="h-6 w-6 group-hover:text-yellow-500 transition duration-500 ease-in-out"/>
+        </a>
+        <a href="https://www.facebook.com/oleg510bodyan/"
+           class="group flex items-center p-[8px] rounded-full shadow-black m-3 hover:shadow-lg bg-white transition duration-500 ease-in-out"
+           target="_blank">
+          <font-awesome-icon :icon="['fab', 'facebook-f']" class="h-6 w-6 group-hover:text-[#3b5998] transition duration-500 ease-in-out"/>
+        </a>
+      </div>
+      <h4 class="text-white text-xs">All rights reserved <font-awesome-icon :icon="['far', 'copyright']" class="w-3 h-3"/> 2024</h4>
+
+
     </div>
   </footer>
 </template>
